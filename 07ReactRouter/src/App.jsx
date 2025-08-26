e3edbaa0096995334188222d6bb13cbf6133615b
mrjaini <aj230074@gmail.com>
2025-08-26 21:40:51 +0530

    Router Learning Project

---
+++ 07ReactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-900 text-green-400 text-2xl font-bold'>React Router</h1>
+    </>
+  )
+}
+
+export default App
